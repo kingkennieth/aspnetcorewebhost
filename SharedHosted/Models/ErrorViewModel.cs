c83c427f67c8d93d0aebf5eefe02be1f04a86e15
Shahrul <shahrul.izzam@gmail.com>
2018-07-15 22:35:27 +0800

    Add project files.

---
+++ SharedHosted/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SharedHosted.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
